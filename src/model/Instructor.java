eec27b1032a433c274e54a6cefee16a5c72c6f93
Даниэл <danielmuktarbekov99@gmail.com>
2026-02-19 00:03:49 +0600

    addedStudentik

---
+++ src/model/Instructor.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Instructor {
+    private String name;
+    private int age;
+
+    public Instructor(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
